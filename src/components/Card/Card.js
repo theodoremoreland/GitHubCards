b81e83b52c7cad49be50073a19ac4ad5924e56b5
Theodore Moreland <theodoremoreland@yahoo.com>
2020-12-18 04:54:46 -0600

    Added more data to profile cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 // Semantic UI
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image } from 'semantic-ui-react';
+
+// Custom styles
+import './Card.css';
 
 class card extends React.Component {
 
@@ -9,16 +12,52 @@ class card extends React.Component {
       let profile = this.props;
 
       return (
-        <Card className="profile">
-          <Image src={profile.avatar_url} alt={profile.alt}/>
-          <Card.Content>
-            <Card.Header>{profile.name}</Card.Header>
+        <Card className="profile" color="teal">
+          <a
+            href={profile.html_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image className="profileImage" src={profile.avatar_url} alt={profile.alt} />
+          </a>
+          <Card.Content textAlign="center">
+            <Card.Header className="profileHeader">{profile.name}</Card.Header>
+            <Card.Meta>
+              <span className='profileJoinDate'>Joined on {new Date(profile.created_at).toLocaleDateString()}</span>
+            </Card.Meta>
+            <Card.Description className="profileBio">{profile.bio}</Card.Description>
+            <Card.Meta>
+              <div>
+                <Icon className="profileFolderIcon" name='folder open' />
+                {profile.public_repos} public repos
+              </div>
+              <div>
+                <Icon className="profileUsersIcon" name='users' />
+                {profile.followers} followers
+                * {profile.following} following
+              </div>
+            </Card.Meta>
           </Card.Content>
           <Card.Content extra>
-            <div>
-              <Icon name='building' />
-                {profile.company}
-            </div>
+          {
+              profile.location
+                ?
+                  <div>
+                    <Icon className="profileLocationIcon" name='location arrow' />
+                      {profile.location}
+                  </div>
+                : ""
+            }
+            {
+              profile.company
+                ?
+                  <div>
+                    <Icon className="profileBuildingIcon" name='building' />
+                      {profile.company}
+                  </div>
+                : ""
+            }
+
           </Card.Content>
         </Card>
       )
